0b675ceb2a38885a1ef6c408e3fec5e53a2fbab6
Milton0123 <maletta.milton@gmail.com>
2023-08-10 15:47:21 -0300

    update

--- app/src/main/java/com/example/refactoringlife4/LoginActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/LoginActivity.kt
@@ -24,7 +24,7 @@ class LoginActivity : AppCompatActivity() {
         setup()
         screenSplash.setKeepOnScreenCondition { false }
     }
-    
+
     private fun setup() {
 
         binding.btLoginGoogle.setOnClickListener {
