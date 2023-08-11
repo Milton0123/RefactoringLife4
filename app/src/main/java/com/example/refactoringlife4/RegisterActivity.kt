23f607ae0fb25bbcab98f889f2556960024271af
cristian <cristian_fabian_pesse@hotmail.com>
2023-08-11 19:38:03 -0300

    Agregue RegisterViewModel

--- app/src/main/java/com/example/refactoringlife4/RegisterActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/RegisterActivity.kt
@@ -7,18 +7,28 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
 import androidx.core.util.PatternsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.refactoringlife4.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    lateinit var binding: ActivityRegisterBinding
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var viewModel: RegisterViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        viewModel = ViewModelProvider(this).get(RegisterViewModel::class.java)
+
         goToBack()
         setupTextWatchers()
         setupRegisterButton()
+
+        viewModel.isRegistrationValid.observe(this, Observer { isValid ->
+            binding.btRegister.isEnabled = isValid
+        })
     }
 
     private fun goToBack() {
@@ -30,54 +40,44 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun setupTextWatchers() {
-        binding.etRegisterName.addTextChangedListener(createTextWatcher(binding.etRegisterName))
-        binding.etRegisterEmail.addTextChangedListener(createTextWatcher(binding.etRegisterEmail))
-        binding.etRegisterPassword.addTextChangedListener(createTextWatcher(binding.etRegisterPassword))
+        val fields =
+            listOf(binding.etRegisterName, binding.etRegisterEmail, binding.etRegisterPassword)
+
+        fields.forEach { editText ->
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                    // No se necesita implementar aquí
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    // No se necesita implementar aquí
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    val name = binding.etRegisterName.text.toString().trim()
+                    val email = binding.etRegisterEmail.text.toString().trim()
+                    val password = binding.etRegisterPassword.text.toString().trim()
+
+                    viewModel.validateFields(name, email, password)
+                }
+            })
+        }
     }
 
     private fun setupRegisterButton() {
         binding.btRegister.isEnabled = false
 
         binding.btRegister.setOnClickListener {
-            if (areFieldsValid()) {
-                // Realizar el registro aquí
-            }
+            val name = binding.etRegisterName.text.toString().trim()
+            val email = binding.etRegisterEmail.text.toString().trim()
+            val password = binding.etRegisterPassword.text.toString().trim()
+
+            viewModel.validateFields(name, email, password)
         }
-    }
-
-    private fun createTextWatcher(view: View): TextWatcher {
-        return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // No se necesita implementar aquí
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                binding.btRegister.isEnabled = areFieldsValid()
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-                // No se necesita implementar aquí
-            }
-        }
-    }
-
-    private fun areFieldsValid(): Boolean {
-        val name = binding.etRegisterName.text.toString().trim()
-        val nameWords = name.split("\\s+".toRegex())
-        val nameValid =
-            name.isNotEmpty() && name.length >= 3 && name.length <= 30 && nameWords.size <= 2
-                    && !name.contains("\\s{2,}".toRegex()) // Verificar que no haya más de un espacio en el medio
-
-        val email = binding.etRegisterEmail.text.toString().trim()
-        val emailValid = email.isNotEmpty() && email.length <= 30 && isValidEmail(email)
-
-        val password = binding.etRegisterPassword.text.toString().trim()
-        val passwordValid = password.isNotEmpty() && password.length >= 6 && password.length <= 30
-
-        return nameValid && emailValid && passwordValid
-    }
-
-    private fun isValidEmail(email: String): Boolean {
-        return PatternsCompat.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
