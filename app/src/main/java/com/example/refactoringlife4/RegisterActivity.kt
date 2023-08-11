a0f9763b3e69d431baf8e1e2982041fd2963e14c
Emilce Cano <emilce.cano95@gmail.com>
2023-08-10 22:00:03 -0300

    create logic register

--- app/src/main/java/com/example/refactoringlife4/RegisterActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/RegisterActivity.kt
@@ -2,10 +2,39 @@ package com.example.refactoringlife4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.refactoringlife4.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        register()
+    }
+
+    fun register() {
+        val email: String = binding.etRegisterEmail.text.toString()
+        val password: String = binding.etRegisterPassword.text.toString()
+
+        binding.btRegister.setOnClickListener {
+            if(binding.etRegisterEmail.text.isNotEmpty() && binding.etRegisterName.text.isNotEmpty()
+                && binding.etRegisterPassword.text.isNotEmpty())
+
+            FirebaseAuth.getInstance()
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Toast.makeText(this, "succes", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "error", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
     }
 }
