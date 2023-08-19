ffb6dd89b63b493f973dd169db1e5ac66ceafd89
cristian <cristian_fabian_pesse@hotmail.com>
2023-08-19 17:16:48 -0300

    Creacion de animacion en flecha de onboarding

--- app/src/main/java/com/example/refactoringlife4/ui/onBoarding/presenters/OnBoardingActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/ui/onBoarding/presenters/OnBoardingActivity.kt
@@ -2,16 +2,24 @@ package com.example.refactoringlife4.ui.onBoarding.presenters
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import com.example.refactoringlife4.R
+import com.example.refactoringlife4.databinding.ActivityOnboardingBinding
 import com.example.refactoringlife4.ui.login.LoginActivity
 import com.example.refactoringlife4.utils.Utils
 
 
-class OnBoardingActivity: AppCompatActivity() {
+class OnBoardingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_onboarding)
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val moveArrowAnimation = AnimationUtils.loadAnimation(this, R.anim.mov_arrow)
+        binding.onboardArrow.startAnimation(moveArrowAnimation)
     }
+
     private fun goToBack() {
         Utils.startActivityWithSlideToRight(this, LoginActivity::class.java, null)
         finish()
