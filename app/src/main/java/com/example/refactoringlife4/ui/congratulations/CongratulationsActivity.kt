e68e3fa41de15f31b1273d92445a34a4d23cc206
Milton0123 <maletta.milton@gmail.com>
2023-08-19 21:55:50 -0300

    create onclick congratulation to onboarding screen

--- app/src/main/java/com/example/refactoringlife4/ui/congratulations/CongratulationsActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/ui/congratulations/CongratulationsActivity.kt
@@ -3,6 +3,8 @@ package com.example.refactoringlife4.ui.congratulations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.refactoringlife4.databinding.ActivityCongratulationsBinding
+import com.example.refactoringlife4.ui.onBoarding.presenters.OnBoardingActivity
+import com.example.refactoringlife4.utils.Utils
 
 class CongratulationsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCongratulationsBinding
@@ -10,5 +12,12 @@ class CongratulationsActivity : AppCompatActivity() {
         binding = ActivityCongratulationsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        goToOnboard()
+    }
+
+    private fun goToOnboard() {
+        binding.root.setOnClickListener {
+            Utils.startActivityWithSlideToRight(this, OnBoardingActivity::class.java, null)
+        }
     }
 }
