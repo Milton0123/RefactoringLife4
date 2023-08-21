43d3a4b64a4765b1ab0f853da377e62e92561178
jereswaxd <jeresanmartin820@gmail.com>
2023-08-21 11:25:41 -0300

    Menu amburguesa creado
    Se creo el menu amburguesa con 2 fragment para utilizar en home, tambien se hizo un pequeño cambio en loginActivity para que envie  al usuario a onboarding
    tambien se eliminaron los menus inecesarios del fragment

---
+++ app/src/main/java/com/example/refactoringlife4/ui/home/presenter/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.refactoringlife4.ui.home.presenter
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.refactoringlife4.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater,container, false)
+
+
+        return binding.root
+    }
+
+
+}
