00b73694ae229f6261020de303020f0f9168125c
Milton0123 <maletta.milton@gmail.com>
2023-08-22 14:20:47 -0300

    update

--- app/src/main/java/com/example/refactoringlife4/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/refactoringlife4/ui/login/LoginActivity.kt
@@ -26,7 +26,6 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setup()
-
         screenSplash.setKeepOnScreenCondition { false }
     }
 
