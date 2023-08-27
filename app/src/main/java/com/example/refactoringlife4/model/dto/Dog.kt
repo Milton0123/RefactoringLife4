c2b5e3aaeebd7db0700b247c8a891f387b305bf7
cristian <cristian_fabian_pesse@hotmail.com>
2023-08-27 13:28:20 -0300

    Cree funcion initRecyclerview, adapter y onclick con toast y imagenes de perros hardcodeadas para probar y deje funcionando

---
+++ app/src/main/java/com/example/refactoringlife4/model/dto/Dog.kt
@@ -0,0 +1,5 @@
+package com.example.refactoringlife4.model.dto
+
+///
+data class Dog(val name: String, val image: Int)
+// creada para harcodear perros en recyclerview
--- app/src/main/java/com/example/refactoringlife4/ui/home/presenter/HomeFragment.kt
+++ app/src/main/java/com/example/refactoringlife4/ui/home/presenter/HomeFragment.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.refactoringlife4.R
 import com.example.refactoringlife4.databinding.FragmentHomeBinding
+import com.example.refactoringlife4.model.dto.Dog
+import com.example.refactoringlife4.ui.home.adapter.HomeFragmentAdapter
 
 class HomeFragment : Fragment() {
 
@@ -20,7 +22,33 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        initRecyclerView()
+
         return binding.root
     }
 
+    private fun initRecyclerView() {
+        val listOfDogs = createListOfDogs()
+        val dogsAdapter = HomeFragmentAdapter(listOfDogs)
+
+        binding.homeRvDogs.apply {
+            adapter = dogsAdapter
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+    }
+
+    private fun createListOfDogs(): List<Dog> {
+        return listOf(
+            // creada para harcodear perros en recyclerview
+            Dog("Dog 1", R.drawable.img_dog_adoptme),
+            Dog("Dog 2", R.drawable.img_dog_adoptme),
+            Dog("Dog 3", R.drawable.img_dog_adoptme),
+            Dog("Dog 4", R.drawable.img_dog_adoptme),
+            Dog("Dog 5", R.drawable.img_dog_adoptme),
+            Dog("Dog 6", R.drawable.img_dog_adoptme),
+            Dog("Dog 7", R.drawable.img_dog_adoptme),
+            Dog("Dog 8", R.drawable.img_dog_adoptme)
+        )
+    }
 }
